45890fa79174c0e72d9f532b6bbc704e62d4c3c8
Steve Hanson <s.hanson5@gmail.com>
2018-03-30 15:58:22 -0500

    Fix meetup service spec

--- spec/services/meetup_service_spec.rb
+++ spec/services/meetup_service_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 describe MeetupService do
-  let(:events_json) { JSON.parse(File.read("spec/fixtures/meetup_events_response.json")) }
+  let(:events_response) { File.read("spec/fixtures/meetup_events_response.json") }
   
   describe ".fetch_upcoming_event" do
     it "returns a single event" do
-      expect(HTTParty).to receive(:get).and_return double('response', body: events_json)
+      expect(HTTParty).to receive(:get).and_return double('response', body: events_response)
       event = MeetupService.fetch_upcoming_event
-      expect(event.name).to eq events_json.first["name"]
+      expect(event.name).to eq JSON.parse(events_response).first["name"]
     end
   end
 end
