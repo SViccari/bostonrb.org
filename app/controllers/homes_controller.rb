194ee9409e7eaeb44c566c8e23492f69c39284dd
Steve Hanson <s.hanson5@gmail.com>
2018-03-30 15:54:21 -0500

    Add caching

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 class HomesController < ApplicationController
   def show
-    @upcoming_event = MeetupService.fetch_upcoming_event
+    @upcoming_event = fetch_upcoming_event
+  end
+  
+  private
+  
+  def fetch_upcoming_event
+    Rails.cache.fetch("upcoming_meetup_event", expires_in: 2.hours) do
+      MeetupService.fetch_upcoming_event
+    end
   end
 end
